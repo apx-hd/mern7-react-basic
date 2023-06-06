ce060ba5057fd71ee733d8091183c1f8a698e17d
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2023-06-06 18:14:13 +0545

    Todo layout

--- src/containers/TodoLayout/index.js
+++ src/containers/TodoLayout/index.js
@@ -1,5 +1,35 @@
+import Button from "../../components/Button";
+import "./todolayout.css";
+import { useState } from "react";
+
 function TodoLayout() {
-    return <></>
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {};
+
+  return (
+    <div className="container">
+      <div className="header">Todo</div>
+      <div className="input-group">
+        <input
+          type="text"
+          className="todoText"
+          placeholder="What to do today?"
+        ></input>
+        <Button color="green">Add</Button>
+      </div>
+      <div className="task-group">
+        {todos.map((todo) => {
+          return (
+            <div className="task-list">
+              <div className="task">{todo}</div>
+              <Button color="red">Delete</Button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default TodoLayout
+export default TodoLayout;
